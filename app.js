02b9ce61ca12cf25e4ff20abc5cffdd6b00788d0
Frances Woodall <franceswoodall@hotmail.co.uk>
2025-12-14 10:48:52 +0000

    JS logic updated with basic game flow functions and event listeners

--- app.js
+++ app.js
@@ -10,10 +10,13 @@
 /*-------------------------------- Constants ----------------------------------*/
 const Board = 0; 
 
+const maxWordLength = 5; 
+
+const maxRows = 6; 
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let btn; 
+let key; 
 
 let winningWord; 
 
@@ -23,15 +26,48 @@ let gameState;
 
 let letterStatus; 
 
-let currentRow; 
+let currentRow = 0; 
 
-let currentTile; 
+let currentTileIndex = 0; 
 
 /*------------------------ Cached Element References ------------------------*/
 
-const alphabetBtns = document.querySelectorAll('.key'); 
+const keyBtns = document.querySelectorAll('.key'); 
+
+const enterBtn = document.querySelector('#enter');
+
+const backspaceBtn = document.querySelector('#backspace'); 
+
+const messageEl = document.querySelector('#game-message'); 
 
 /*-------------------------------- Functions --------------------------------*/
+
+const updateGrid = (letter) => {
+    const tileId = `tile-${currentRow}-${currentTileIndex}`; 
+    const currentTile = document.getElementById(tileId); 
+    if (currentTile && currentTileIndex <5) {
+        currentTile.textContent = letter; 
+        currentTileIndex++; 
+    }
+}; 
+
+const clickEnter = () => {
+    if (currentTileIndex === maxWordLength) {
+        currentRow ++; 
+        currentTileIndex = 0; 
+    } else {
+        updateMessage(); 
+    }
+}; 
+
+const updateMessage = () => {
+    if (currentTileIndex < maxWordLength) {
+        messageEl.textContent = `Not enough letters in this row!`; 
+        setTimeout(() => {
+            messageEl.textContent = ''; 
+        }, 2000); 
+    }
+}
 
 /*----------------------------- Event Listeners ----------------------------*/
 
@@ -39,10 +75,22 @@ const alphabetBtns = document.querySelectorAll('.key');
 // the player needs to be able to click any letter button
 // the letter button needs to appear on the grid 
 
-alphabetBtns.forEach(btn => {
+keyBtns.forEach(btn => {
     btn.addEventListener('click', (evt) => {
         const clickedBtnElement = evt.target;
         const letterAsString = clickedBtnElement.textContent; 
-        // console.log('clicked letter as string', letterAsString); 
+        updateGrid(letterAsString); 
+        console.log('clicked letter as string', letterAsString); 
     }); 
 }); 
+
+enterBtn.addEventListener('click', (evt) => {
+     console.log('Enter key clicked'); 
+     clickEnter(); 
+}); 
+
+backspaceBtn.addEventListener('click', (evt) => {
+    console.log('Backspace key clicked'); 
+}); 
+       
+
